165a4409bd974c9aab705726631ea472067f258b
Shyam Sunder Grover <post2shyam@gmail.com>
2020-11-28 18:19:12 +0530

    Auth user is now allowed to create a new question
    Auth user is now allowed to create a new question

--- quora-service/src/main/java/com/upgrad/quora/service/business/QuestionBusinessService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/QuestionBusinessService.java
@@ -2,7 +2,8 @@ package com.upgrad.quora.service.business;
 
 import com.upgrad.quora.service.dao.QuestionDao;
 import com.upgrad.quora.service.entity.QuestionEntity;
-import com.upgrad.quora.service.entity.UserEntity;
+import com.upgrad.quora.service.entity.UserAuthEntity;
+import com.upgrad.quora.service.exception.AuthorizationFailedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -15,9 +16,13 @@ public class QuestionBusinessService {
     private QuestionDao questionDao;
 
     @Transactional(propagation = Propagation.REQUIRED)
-    public QuestionEntity createQuestion(final QuestionEntity questionEntity, final String authorization) {
-        //TODO handle authorization of the user, and associating authorized user to the question
-        questionEntity.setUser(new UserEntity());
+    public QuestionEntity createQuestion(final QuestionEntity questionEntity, final String authorization) throws AuthorizationFailedException {
+        UserAuthEntity userAuthToken = questionDao.getUserAuthToken(authorization);
+        if (userAuthToken == null) {
+            throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
+        }
+
+        questionEntity.setUser(userAuthToken.getUserEntity());
         return questionDao.createQuestion(questionEntity);
     }
 }
