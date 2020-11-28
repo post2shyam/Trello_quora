16587deebd020986b022a4c71e23fd022f740359
shashankm@exzeo.com <shashankm@exzeo.com>
2020-11-28 18:48:20 +0530

    Add create answer logic

--- quora-api/src/main/java/com/upgrad/quora/api/controller/AnswerController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/AnswerController.java
@@ -5,6 +5,7 @@ import com.upgrad.quora.api.model.AnswerResponse;
 import com.upgrad.quora.service.business.AnswerBusinessService;
 import com.upgrad.quora.service.business.UserBusinessService;
 import com.upgrad.quora.service.entity.AnswerEntity;
+import com.upgrad.quora.service.entity.UserAuthEntity;
 import com.upgrad.quora.service.entity.UserEntity;
 import com.upgrad.quora.service.exception.AuthorizationFailedException;
 import com.upgrad.quora.service.exception.InvalidQuestionException;
@@ -18,18 +19,36 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/")
 public class AnswerController {
 
-        @Autowired
-        private AnswerBusinessService answerBusinessService;
+    @Autowired
+    private AnswerBusinessService answerBusinessService;
 
-        @RequestMapping(method = RequestMethod.POST, path = "/question/{questionId}/answer/create", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-        public ResponseEntity<AnswerResponse> createAnswer(@RequestHeader("authorization") final String authorization, @PathVariable("questionId") final String questionId, final AnswerRequest answerRequest) {
+    @Autowired
+    private UserBusinessService userBusinessService;
 
-            AnswerEntity answerEntity = new AnswerEntity();
+    @Autowired
+    private QuestionBusinessService questionBusinessService;
 
-            AnswerEntity createdAnswer = answerBusinessService.createAnswer(answerEntity, userAuthEntity);
-            AnswerResponse answerResponse = new AnswerResponse().id('database_uuid1').
-                    status("ANSWER CREATED");
+    @RequestMapping(method = RequestMethod.POST, path = "/question/{questionId}/answer/create", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<AnswerResponse> createAnswer(@RequestHeader("authorization") final String authorization, @PathVariable("questionId") final String questionId, final AnswerRequest answerRequest)
+            throws AuthorizationFailedException, InvalidQuestionException {
 
-            return new ResponseEntity<AnswerResponse>(answerResponse, HttpStatus.OK);
-        }
+        final UserAuthEntity userAuthEntity = userBusinessService.getUser(authorization);
+        QuestionEntity questionEntity = questionBusinessService.validateQuestion(questionId);
+
+        UserEntity userEntity = userAuthEntity.getUser();
+
+        AnswerEntity answerEntity = new AnswerEntity();
+        answerEntity.setUuid(UUID.randomUUID().toString());
+        answerEntity.setDate(ZonedDateTime.now());
+        answerEntity.setQuestion(questionEntity);
+        answerEntity.setAnswer(answerRequest.getAnswer());
+        answerEntity.setUser(userEntity);
+
+        AnswerEntity createdAnswer = answerBusinessService.createAnswer(answerEntity, userAuthEntity);
+        AnswerResponse answerResponse = new AnswerResponse().id(createdAnswer.getUuid()).
+                status("ANSWER CREATED");
+
+        return new ResponseEntity<AnswerResponse>(answerResponse, HttpStatus.OK);
+    }
+
 }
--- quora-service/src/main/java/com/upgrad/quora/service/business/AnswerBusinessService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/AnswerBusinessService.java
@@ -1,5 +1,26 @@
 package com.upgrad.quora.service.business;
 
+import com.upgrad.quora.service.dao.AnswerDao;
+import com.upgrad.quora.service.entity.AnswerEntity;
+import com.upgrad.quora.service.entity.UserAuthEntity;
+import com.upgrad.quora.service.exception.AuthorizationFailedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
 public class AnswerBusinessService {
 
+    @Autowired
+    private AnswerDao answerDao;
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public AnswerEntity createAnswer(AnswerEntity answerEntity , UserAuthEntity userAuthEntity) throws AuthorizationFailedException {
+
+        if (userAuthEntity.getLogoutAt()!=null){
+            throw new AuthorizationFailedException("ATHR-002", "User is signed out.Sign in first to post an answer");
+        }
+        return answerDao.createAnswer(answerEntity);
+    }
 }
