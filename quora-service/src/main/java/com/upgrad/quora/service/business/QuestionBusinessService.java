93c43176f79910a799e9433f6689e388be6bee38
Shyam Sunder Grover <post2shyam@gmail.com>
2020-11-28 16:30:51 +0530

    Add support code for create question feature

---
+++ quora-service/src/main/java/com/upgrad/quora/service/business/QuestionBusinessService.java
@@ -0,0 +1,23 @@
+package com.upgrad.quora.service.business;
+
+import com.upgrad.quora.service.dao.QuestionDao;
+import com.upgrad.quora.service.entity.QuestionEntity;
+import com.upgrad.quora.service.entity.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class QuestionBusinessService {
+
+    @Autowired
+    private QuestionDao questionDao;
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public QuestionEntity createQuestion(final QuestionEntity questionEntity, final String authorization) {
+        //TODO handle authorization of the user, and associating authorized user to the question
+        questionEntity.setUser(new UserEntity());
+        return questionDao.createQuestion(questionEntity);
+    }
+}
