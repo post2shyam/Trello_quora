b2ae732b7db70789aaa96f7f4f998180a68c513a
shashankm@exzeo.com <shashankm@exzeo.com>
2020-11-29 16:18:46 +0530

    Add code level improvements

--- quora-service/src/main/java/com/upgrad/quora/service/dao/AnswerDao.java
+++ quora-service/src/main/java/com/upgrad/quora/service/dao/AnswerDao.java
@@ -29,7 +29,7 @@ public class AnswerDao {
         }
     }
 
-    public Boolean isUserOwnerOfAnswer(String answerUuid, String userUuid) {
+    public Boolean isUserOwnerOfAnswer(final String answerUuid, final String userUuid) {
 
         try {
             AnswerEntity answerEntity = entityManager.createNamedQuery("validateOwnership", AnswerEntity.class).setParameter("answerUuid", answerUuid).setParameter("userUuid", userUuid).getSingleResult();
